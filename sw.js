2e983a33d0a66e8415a7d7051cbbbd2a61c6e20c
Hunglung222 <kevin67222@gmail.com>
2026-05-08 09:33:51 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'fb-v2026.05.07-1735';
+const V = 'fb-v2026.05.08-0929';
 const A = [
   './login.html','./add.html','./index.html','./report.html',
   './wallet.html','./settings.html','./shopping.html','./private.html','./memo.html',
